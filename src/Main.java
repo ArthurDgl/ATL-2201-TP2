638d175c6a0cad4162b2550797efe768be7f595a
Arthur <arthur.dugnolle@gmail.com>
2024-10-09 13:36:40 +0200

    Run Tests in Main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 public class Main {
     public static void main(String[] args) {
-//        Vecteur v1 = new Vecteur(new double[]{2, 0, 1, 4});
-//        Vecteur v2 = new Vecteur(new double[]{0,2,0,2});
-
-//        Vecteur v1 = new Vecteur(new double[]{1, 1, 0});
-//        Vecteur v2 = new Vecteur(new double[]{-4, -4, 0});
-
-        Vecteur v1 = new Vecteur(new double[]{0, 1});
-        Vecteur v2 = new Vecteur(new double[]{0, 0});
-
-        System.out.println(v1.estColineaire(v2));
+        Result result = JUnitCore.runClasses(TestsUnitairesTP2_5.class);
+        System.out.println("Tests validés : " + (result.getRunCount() - result.getFailureCount()));
+        System.out.println("Tests non validés :");
+        for (Failure failure : result.getFailures()) {
+            System.out.println("   - " + failure.getDescription() + " : " + failure.getMessage());
+        }
     }
 }
